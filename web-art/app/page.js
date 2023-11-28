5a6226ea8bb89f9b34681b1a0a3db0d23fd803d4
javadBoroji <javadboroji2222@gmail.com>
2023-11-28 11:42:53 +0330

    add initial Header-talwind

--- web-art/app/page.js
+++ web-art/app/page.js
@@ -1,8 +1,10 @@
 import Image from 'next/image'
 import MainStyle from '@/app/MainStyle.css'
+import HeaderCs from './Components/HeaderCs'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <HeaderCs />
       <h1> WebArt</h1>
     </main>
   )
